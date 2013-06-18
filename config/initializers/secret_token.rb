c67e190eab9446f4faaa773cd87f5d6270dd120d
Adam Louis <adamlouis@gmail.com>
2013-06-17 22:17:34 -0700

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HerokuPoltergeistTest::Application.config.secret_token = 'e894a6e660fe2ed1b7f28eb2e23decbea6110e16b562be85ae8f243195b8ef105a92f572eb41641c0c1a8dcf93642b9680ed12031165f67471a2ea7be16a394e'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+HerokuPoltergeistTest::Application.routes.draw do
+  root :to => "home#index"
+end
